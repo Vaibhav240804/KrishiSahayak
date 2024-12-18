c024e30e25ade231e4f1d23d25223f0197837b88
Vaibhav Kore <101107216+Vaibhav240804@users.noreply.github.com>
2024-12-18 17:22:53 +0530

    Update database.js

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   const { connection } = await mongoose.connect(
-    "mongodb+srv://direct2vaibhavkore:C18EQUODpPZPZbnB@cluster0.b7y0h.mongodb.net/?retryWrites=true&w=majority&appName=cluster0"
+    "mongodb+srv://username:pass@cluster0.b7y0h.mongodb.net/?retryWrites=true&w=majority&appName=cluster0"
   );
   console.log(`Mongo connected with ${connection.host}`);
 };
